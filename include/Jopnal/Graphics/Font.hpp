90da60909987f1bd197391514145f734b6be605e
Ville Koskinen <ville.koskinen94@hotmail.fi>
2016-02-05 14:15:19 +0200

    - Cleaned up code - Added commenting

--- include/Jopnal/Graphics/Font.hpp
+++ include/Jopnal/Graphics/Font.hpp
@@ -1,23 +1,21 @@
-// Jopnal Engine C++ Library
-// Copyright(c) 2016 Team Jopnal
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+//Jopnal Engine C++ Library
+//Copyright(c) 2016 Team Jopnal
+//
+//This software is provided 'as-is', without any express or implied
+//warranty.In no event will the authors be held liable for any damages
+//arising from the use of this software.
+//
+//Permission is granted to anyone to use this software for any purpose,
+//including commercial applications, and to alter it and redistribute it
+//freely, subject to the following restrictions :
+//
+//1. The origin of this software must not be misrepresented; you must not
+//claim that you wrote the original software.If you use this software
+//in a product, an acknowledgement in the product documentation would be
+//appreciated but is not required.
+//2. Altered source versions must be plainly marked as such, and must not be
+//misrepresented as being the original software.
+//3. This notice may not be removed or altered from any source distribution.
 
 //////////////////////////////////////////////
 
@@ -27,14 +25,13 @@
 // Headers
 #include <Jopnal/Header.hpp>
 #include <Jopnal/Core/Resource.hpp>
-#include <memory>
-
-#include <Jopnal/Graphics/stb/stb_truetype.h>
+#include <../src/Jopnal/Graphics/stb/stb_truetype.h>
 #include <Jopnal/MathInclude.hpp>
-
+#include <memory>
 #include <unordered_map>
 
 //////////////////////////////////////////////
+
 
 struct stbtt_fontinfo;
 
@@ -50,17 +47,54 @@ namespace jop
         :public Resource
 	{
 	public:
-        bool load(const std::string& path)override;
+
+        /// \brief Loads a font from targeted path
+        ///
+        /// \param path Path to desired .ttf font file
+        ///
+        bool load(const std::string& path);
+
+        /// \brief Calculates font boundaries
+        ///
+        /// \param codepoint Numerical value pointing to a single character
+        ///
         std::pair<glm::ivec2, glm::ivec2> getBounds(const int codepoint);
+
+        /// \brief Returns the necessary kerning between characters
+        ///
+        /// \param codepoint1 Numerical value pointing to the desired character
+        /// \param codepoint2 Ditto
+        ///
         int getKerning(const int codepoint1, const int codepoint2);
+
+        /// \brief Checks if glyph is in bitmap
+        ///
+        /// \param scaleX Horizontal scale of a single character
+        /// \param scaleY Vertical scale of a single character
+        /// \param codepoint Pointer to a single character
+        /// \param width Width of a single character
+        /// \param height Height of a single character
+        ///
         unsigned char* getCodepointBitmap(const float scaleX, const float scaleY, const int codepoint, int* width, int* height);
+        
+        /// \brief Deconstructor
+        ///
+        ///
         ~Font();
+
 	private:
-        std::unique_ptr<stbtt_fontinfo> m_info; //font info
-        //std::unordered_map <int, std::pair<unsigned char*, glm::uvec2> > m_bitmaps; //codepoint, data, size
-        std::unordered_map <int, Bitmap> m_bitmaps;
+
+        std::unique_ptr<stbtt_fontinfo> m_info; ///< Font info
+        std::unordered_map <int, Bitmap> m_bitmaps; ///< Bitmaps
+
 	};
 }
 
 #endif
 
+/// \class Font
+/// \ingroup graphics
+///
+/// Font manager class which loads any desired TrueType font file (.ttf)
+/// 
+///
--- src/Jopnal/Graphics/Font.cpp
+++ src/Jopnal/Graphics/Font.cpp
@@ -1,34 +1,31 @@
-// Jopnal Engine C++ Library
-// Copyright(c) 2016 Team Jopnal
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+//Jopnal Engine C++ Library
+//Copyright(c) 2016 Team Jopnal
+//
+//This software is provided 'as-is', without any express or implied
+//warranty.In no event will the authors be held liable for any damages
+//arising from the use of this software.
+//
+//Permission is granted to anyone to use this software for any purpose,
+//including commercial applications, and to alter it and redistribute it
+//freely, subject to the following restrictions :
+//
+//1. The origin of this software must not be misrepresented; you must not
+//claim that you wrote the original software.If you use this software
+//in a product, an acknowledgement in the product documentation would be
+//appreciated but is not required.
+//2. Altered source versions must be plainly marked as such, and must not be
+//misrepresented as being the original software.
+//3. This notice may not be removed or altered from any source distribution.
 
 //////////////////////////////////////////////
+
+#define STB_TRUETYPE_IMPLEMENTATION
 
 // Headers
 #include <Jopnal/Precompiled.hpp>
 
-#define STB_TRUETYPE_IMPLEMENTATION
-
-#include <Jopnal/Graphics/stb/stb_truetype.h>
-
 //////////////////////////////////////////////
+
 
 namespace jop
 {
@@ -47,14 +44,14 @@ namespace jop
     {
         auto info_ptr = std::make_unique<stbtt_fontinfo>();
 
-        //load font data from file
+        // Load font data from file
         std::vector<unsigned char> buffer;
 
         FileLoader::read(path, buffer);
 
         if (!buffer.empty())
         {
-            //save data
+            // Save loaded data
             bool success = stbtt_InitFont(info_ptr.get(), buffer.data(), 0);
             JOP_ASSERT(success, "Font failed!");
 
@@ -70,7 +67,7 @@ namespace jop
     {
         int x0, y0, x1, y1;
         stbtt_GetCodepointBox(m_info.get(), codepoint, &x0, &y0, &x1, &y1);
-        return std::make_pair(glm::ivec2(x0, y0), glm::ivec2(x1-x0, y1-y0)); //x, y, w, h
+        return std::make_pair(glm::ivec2(x0, y0), glm::ivec2(x1-x0, y1-y0)); // X, Y, width & height
     }
 
     //////////////////////////////////////////////
@@ -87,31 +84,30 @@ namespace jop
         auto it = m_bitmaps.find(codepoint);
         if (it != m_bitmaps.end())
         {
-            //return data of the found glyph bitmap
+            // Return data of the found glyph bitmap
             *width = it->second.width;
             *height = it->second.width;
             return it->second.data;
         }
         else
         {
-            //get glyph rectangle size in pixels
+            // Get glyph rectangle size in pixels
             std::pair<glm::ivec2, glm::ivec2> bounds = getBounds(codepoint);
-            //x, y, width, height
             
-            //create the bitmap
+            // Create a bitmap
             unsigned char* data = stbtt_GetCodepointBitmap(m_info.get(), scaleX, scaleY, codepoint, 
                 &bounds.second.x, &bounds.second.y,
                 &bounds.first.x,
                 &bounds.first.y);
             
-            //add the new bitmap to the map
+            // Add the new bitmap to the map
             Bitmap bm;
             bm.height = bounds.second.x;
             bm.width = bounds.second.y;
             bm.data = data;
             m_bitmaps[codepoint] = bm;
 
-            //return necessary stuff
+            // Return necessary data
             *width = bounds.second.x;
             *height = bounds.second.y;
             return data;
