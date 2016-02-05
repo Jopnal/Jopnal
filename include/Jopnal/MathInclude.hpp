7712be2f0d20740cd0fdb1a449e67b79158f5ba8
Pyry Kosola <pyrykos@AOTA1-1L160-16D.kamit.fi>
2016-02-05 15:21:21 +0200

    Clean up & commenting

--- include/Jopnal/MathInclude.hpp
+++ include/Jopnal/MathInclude.hpp
@@ -31,6 +31,5 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
-#include <glm/gtc/constants.hpp>
 
 #endif
--- src/Jopnal/Graphics/SphereModel.cpp
+++ src/Jopnal/Graphics/SphereModel.cpp
@@ -1,31 +1,26 @@
 // Jopnal Engine C++ Library
-// Copyright(c) 2016 Team Jopnal
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// Copyright (c) 2016 Team Jopnal
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
 
 //////////////////////////////////////////////
 
 // Headers
 #include <Jopnal/Precompiled.hpp>
-
-#define M_PI       3.14159265358979323846
-#define M_PI_2     1.57079632679489661923
 
 //////////////////////////////////////////////
 
@@ -38,38 +33,35 @@ namespace jop
         const float S = 1.0f / (float)(sectors - 1.0f); // Sectors
         std::size_t s, r;
 
-        std::vector<float> positions;
-        std::vector<float> normals;
-        std::vector<float> texcoords;
         std::vector<unsigned int> indices;
 
-        positions.resize(rings * sectors * 3);
-        normals.resize(rings * sectors * 3);
-        texcoords.resize(rings * sectors * 2);
-
-        std::vector<float>::iterator v = positions.begin();
-        std::vector<float>::iterator n = normals.begin();
-        std::vector<float>::iterator t = texcoords.begin();
+        std::vector<Vertex> vertexArray;
+        vertexArray.resize(rings * sectors);
+        auto itr = vertexArray.begin();
 
         for (r = 0; r < rings; ++r)
         {
             for (s = 0; s < sectors; ++s)
             {
-                
-                float const y = sin(-M_PI_2 + M_PI * r * R);
-                float const x = cos(2 * M_PI * s * S) * sin(M_PI * r * R);
-                float const z = sin(2 * M_PI * s * S) * sin(M_PI * r * R);
+                static const float pi1 = glm::pi<float>(); // pi
+                static const float pi2 = glm::half_pi<float>(); // half pi
 
-                *t++ = s*S;
-                *t++ = r*R;
+                float const y = sin(-pi2 + pi1 * r * R);
+                float const x = cos(2 * pi1 * s * S) * sin(pi1 * r * R);
+                float const z = sin(2 * pi1 * s * S) * sin(pi1 * r * R);
 
-                *v++ = x * radius;
-                *v++ = y * radius;
-                *v++ = z * radius;
+                itr->position.x = x * radius;
+                itr->position.y = y * radius;
+                itr->position.z = z * radius;
 
-                *n++ = x;
-                *n++ = y;
-                *n++ = z;
+                itr->texCoords.x = s * S;
+                itr->texCoords.y = r * R;
+
+                itr->normalVector.x = x;
+                itr->normalVector.y = y;
+                itr->normalVector.z = z;
+
+                ++itr;
             }
         }
 
@@ -86,20 +78,6 @@ namespace jop
             }
         }
 
-        //Different way to do this?--------------------------------------
-        std::vector<Vertex> vertexArray;
-        vertexArray.reserve(positions.size() / 3);
-
-        for (std::size_t i = 0; i < vertexArray.capacity(); ++i)
-        {
-            vertexArray.emplace_back
-                (
-                glm::vec3(positions[i * 3], positions[i * 3 + 1], positions[i * 3 + 2]),
-                texcoords.size() > i * 2 ? glm::vec2(texcoords[i * 2], texcoords[i * 2 + 1]) : glm::vec2(),
-                normals.size() > i * 3 ? glm::vec3(normals[i * 3], normals[i * 3 + 1], normals[i * 3 + 2]) : glm::vec3()
-                );
-        }
-        //---------------------------------------------------------
         load(vertexArray, indices);
     }
 
